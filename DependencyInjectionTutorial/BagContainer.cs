b321d4901b8c9090027f5c8d1108c2b6b6c866a4
unknown <elondon@EL-LAPTOPWIN7.home>
2013-10-23 20:46:46 -0400

    added Bag to the configuration to demonstrate passing IBuildObjects as a dependency

--- DependencyInjectionTutorial/BagContainer.cs
+++ DependencyInjectionTutorial/BagContainer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using System.Dynamic;
+using IBuildObjects;
 
 namespace DependencyInjectionTutorial
 {
@@ -8,11 +9,14 @@ namespace DependencyInjectionTutorial
     {
         public Bag[] Bags { get; set; }
 
-        public BagContainer(int numberOfBags, int maxItemsPerBag)
+        private readonly IObjectBuilder _objectBuilder;
+
+        public BagContainer(IObjectBuilder objectBuilder, int numberOfBags)
         {
+            _objectBuilder = objectBuilder;
             Bags = new Bag[numberOfBags];
-            for(var i = 0; i < numberOfBags; i++)
-                Bags[i] = new Bag(maxItemsPerBag);
+            for (var i = 0; i < numberOfBags; i++)
+                Bags[i] = _objectBuilder.GetInstance<Bag>();
         }
     }
 }
--- DependencyInjectionTutorial/Program.cs
+++ DependencyInjectionTutorial/Program.cs
@@ -19,18 +19,13 @@ namespace DependencyInjectionTutorial
 
             // instanciate and configure your player without the use of IoC/DI - as if you just wanted to make a new player at some
             // random point in your code.
-            var playerOne = new Player(100, new Inventory(new BagContainer(5, 10)), new Hud(new Knife()), new Map(new PlayerLocator()));
-
-            // and each time you need to change how one of these objects is configured, you need to find every line like this and change it.
-            // hopefully, you don't make any mistakes. For example, you decide a player is going to start with 90 health instead of 100. Or 
-            // you want players to start with a pistol instead of a knife. Or perhaps your player locator for placing players on the map
-            // has drastically changed.
+            var playerOne = new Player(100, new Inventory(new BagContainer(new ObjectBoss(), 5)), new Hud(new Knife()), new Map(new PlayerLocator()));
 
             // we can clean this up a little:
 
             const int STARTING_HEALTH = 100;
 
-            var newBagContainer = new BagContainer(5, 10);
+            var newBagContainer = new BagContainer(new ObjectBoss(), 5);
             var newInventory = new Inventory(newBagContainer);
             var startingWeapon = new Knife();
             var newHud = new Hud(startingWeapon);
@@ -40,29 +35,21 @@ namespace DependencyInjectionTutorial
             // and finally:
             var playerTwo = new Player(STARTING_HEALTH, newInventory, newHud, playerMap);
 
-            // that is cleaner but still several places to make changes. The old time standard as far as solutions to this problem
-            // is the factory pattern. A class that knows how to configure a player and can produce them whenever you need them.
-            // Nice because now we've moved all of the configuration code to 1 place. Maintenence is much easier.
+            // that is cleaner but still several places to make changes.
 
             var playerThree = PlayerFactory.GetConfiguredPlayer();
-
             // This really is a legitimate solution - but it can get messy when you have several of these factories in your project
             // and eventually the factory itself will need to be configured to have access to the components it needs to build what
             // it produces. 
 
-            // Enter Inversion of Control and Dependency Injection. A dependency is just an object that another object needs to
-            // operate properly. Since the player class needs an Inventory, Hud, and Map to function properly, those objects
-            // are said to be dependencies of Player.
-
+            // Enter Inversion of Control and Dependency Injection. 
             var playerFour = objectBuilder.GetInstance<Player>();
 
             // hover over playerFour and observe the object graph is all connected. In fact, thanks to the configuration,
             // you can get any object you want from the object builder.
-
             var map = objectBuilder.GetInstance<Map>();
 
             // maybe you want to get an instance of every weapon in the game.
-
             var weapons = objectBuilder.GetAllInstances<IWeapon>();
 
             // That is just the tip of the ice berg. Once you configure the IoC tool, you have a factory in one place
@@ -77,7 +64,8 @@ namespace DependencyInjectionTutorial
             {
                 x.Add<Player>().WithCustomConstructor(new Dictionary<string, object>() { { "health", 100 } }); ;
                 x.Add<Inventory>();
-                x.Add<BagContainer>().WithCustomConstructor(new Dictionary<string, object>() { { "numberOfBags", 5 }, { "maxItemsPerBag", 10 } });
+                x.Add<BagContainer>().WithCustomConstructor(new Dictionary<string, object>() { { "numberOfBags", 5 } });
+                x.Add<Bag>().WithCustomConstructor(new Dictionary<string, object>() {{"maxBagItems", 10}});
                 x.AddUsing<IWeapon, Knife>();
                 x.AddUsing<IWeapon, Pistol>();
                 x.Add<PlayerLocator>();
@@ -94,7 +82,7 @@ namespace DependencyInjectionTutorial
         {
             const int STARTING_HEALTH = 100;
 
-            var newBagContainer = new BagContainer(5, 10);
+            var newBagContainer = new BagContainer(new ObjectBoss(), 5);
             var newInventory = new Inventory(newBagContainer);
             var startingWeapon = new Knife();
             var newHud = new Hud(startingWeapon);
